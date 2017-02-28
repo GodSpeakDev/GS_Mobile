2181995252f04611e80a52ae1477471e358b6a12
Ben Bishop <ben@rendr.io>
2017-02-28 13:51:27 -0600

    Setup Android loading screen.

--- GodSpeak.Mobile/Droid/Resources/Resource.designer.cs
+++ GodSpeak.Mobile/Droid/Resources/Resource.designer.cs
@@ -2459,55 +2459,58 @@ namespace GodSpeak.Droid
 			public const int icon = 2130837692;
 			
 			// aapt resource value: 0x7f0200bd
-			public const int mr_dialog_material_background_dark = 2130837693;
+			public const int launch_screen = 2130837693;
 			
 			// aapt resource value: 0x7f0200be
-			public const int mr_dialog_material_background_light = 2130837694;
+			public const int mr_dialog_material_background_dark = 2130837694;
 			
 			// aapt resource value: 0x7f0200bf
-			public const int mr_ic_audiotrack_light = 2130837695;
+			public const int mr_dialog_material_background_light = 2130837695;
 			
 			// aapt resource value: 0x7f0200c0
-			public const int mr_ic_cast_dark = 2130837696;
+			public const int mr_ic_audiotrack_light = 2130837696;
 			
 			// aapt resource value: 0x7f0200c1
-			public const int mr_ic_cast_light = 2130837697;
+			public const int mr_ic_cast_dark = 2130837697;
 			
 			// aapt resource value: 0x7f0200c2
-			public const int mr_ic_close_dark = 2130837698;
+			public const int mr_ic_cast_light = 2130837698;
 			
 			// aapt resource value: 0x7f0200c3
-			public const int mr_ic_close_light = 2130837699;
+			public const int mr_ic_close_dark = 2130837699;
 			
 			// aapt resource value: 0x7f0200c4
-			public const int mr_ic_media_route_connecting_mono_dark = 2130837700;
+			public const int mr_ic_close_light = 2130837700;
 			
 			// aapt resource value: 0x7f0200c5
-			public const int mr_ic_media_route_connecting_mono_light = 2130837701;
+			public const int mr_ic_media_route_connecting_mono_dark = 2130837701;
 			
 			// aapt resource value: 0x7f0200c6
-			public const int mr_ic_media_route_mono_dark = 2130837702;
+			public const int mr_ic_media_route_connecting_mono_light = 2130837702;
 			
 			// aapt resource value: 0x7f0200c7
-			public const int mr_ic_media_route_mono_light = 2130837703;
+			public const int mr_ic_media_route_mono_dark = 2130837703;
 			
 			// aapt resource value: 0x7f0200c8
-			public const int mr_ic_pause_dark = 2130837704;
+			public const int mr_ic_media_route_mono_light = 2130837704;
 			
 			// aapt resource value: 0x7f0200c9
-			public const int mr_ic_pause_light = 2130837705;
+			public const int mr_ic_pause_dark = 2130837705;
 			
 			// aapt resource value: 0x7f0200ca
-			public const int mr_ic_play_dark = 2130837706;
+			public const int mr_ic_pause_light = 2130837706;
 			
 			// aapt resource value: 0x7f0200cb
-			public const int mr_ic_play_light = 2130837707;
-			
-			// aapt resource value: 0x7f0200cd
-			public const int notification_template_icon_bg = 2130837709;
+			public const int mr_ic_play_dark = 2130837707;
 			
 			// aapt resource value: 0x7f0200cc
-			public const int splash = 2130837708;
+			public const int mr_ic_play_light = 2130837708;
+			
+			// aapt resource value: 0x7f0200ce
+			public const int notification_template_icon_bg = 2130837710;
+			
+			// aapt resource value: 0x7f0200cd
+			public const int splash = 2130837709;
 			
 			static Drawable()
 			{
@@ -2849,6 +2852,9 @@ namespace GodSpeak.Droid
 			// aapt resource value: 0x7f0b0020
 			public const int pin = 2131427360;
 			
+			// aapt resource value: 0x7f0b00a5
+			public const int progressBar1 = 2131427493;
+			
 			// aapt resource value: 0x7f0b0007
 			public const int progress_circular = 2131427335;
 			
@@ -2927,8 +2933,8 @@ namespace GodSpeak.Droid
 			// aapt resource value: 0x7f0b0038
 			public const int showTitle = 2131427384;
 			
-			// aapt resource value: 0x7f0b00a5
-			public const int sliding_tabs = 2131427493;
+			// aapt resource value: 0x7f0b00a6
+			public const int sliding_tabs = 2131427494;
 			
 			// aapt resource value: 0x7f0b0079
 			public const int snackbar_action = 2131427449;
@@ -2990,8 +2996,8 @@ namespace GodSpeak.Droid
 			// aapt resource value: 0x7f0b0055
 			public const int title_template = 2131427413;
 			
-			// aapt resource value: 0x7f0b00a6
-			public const int toolbar = 2131427494;
+			// aapt resource value: 0x7f0b00a7
+			public const int toolbar = 2131427495;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int top = 2131427370;
