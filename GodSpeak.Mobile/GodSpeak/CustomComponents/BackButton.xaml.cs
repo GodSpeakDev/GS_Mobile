5b3f3e6cf96cf1952e215d36a4dfdbb0238ab9ee
pauloortins <paulo.ortins@gmail.com>
2017-03-14 22:00:45 -0300

    Back Button

---
+++ GodSpeak.Mobile/GodSpeak/CustomComponents/BackButton.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace GodSpeak
+{
+	public partial class BackButton : StackLayout
+	{
+		public BackButton()
+		{
+			InitializeComponent();
+			this.GestureRecognizers.Add(new TapGestureRecognizer()
+			{
+				Command = new Command(() => 
+				{
+					AnimateView(this, (this.BindingContext as CustomViewModel).CloseCommand, null);
+				})
+			});
+		}
+
+		private void AnimateView(View view, ICommand command, object commandParameter)
+		{
+			var reduceOpacityAnimation = new Animation((x) =>
+			{
+				view.Opacity = 1 - x * .5;
+			}, finished: () =>
+			{
+				command.Execute(commandParameter);
+			});
+
+			var increaseOpacityAnimation = new Animation((x) =>
+			{
+				view.Opacity = 1 - 0.5 + x * .5;
+			});
+
+			var animation = new Animation();
+			animation.Add(0, 0.5, reduceOpacityAnimation);
+			animation.Add(0.5, 1, increaseOpacityAnimation);
+			animation.Commit(view, "Tap");
+		}
+	}
+}
