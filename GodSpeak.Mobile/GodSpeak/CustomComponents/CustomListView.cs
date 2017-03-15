2b55d73a2d1befedeab5bfc6433d99a94e8b02c9
pauloortins <paulo.ortins@gmail.com>
2017-03-15 16:28:01 -0300

    #16:Messages - ListView Selection State

---
+++ GodSpeak.Mobile/GodSpeak/CustomComponents/CustomListView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GodSpeak
+{
+	public class CustomListView : ListView
+	{
+		public CustomListView()
+		{
+			ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
+			{
+				SelectedItem = null;
+			};
+		}
+	}
+}
