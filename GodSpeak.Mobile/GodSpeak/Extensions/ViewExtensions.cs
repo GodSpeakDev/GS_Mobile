80576e3fbb56f4d89acf3ab049b651ed853e9df6
Ben Bishop <ben@rendr.io>
2017-03-15 11:46:57 -0500

    Added extension method to deselect ListView on tap [#16 status:Ready For Retest]

---
+++ GodSpeak.Mobile/GodSpeak/Extensions/ViewExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GodSpeak.Extensions
+{
+    public static class ViewExtensions
+    {
+        public static void DeselectOnTap (this ListView listView)
+        {
+            listView.ItemSelected += (object sender, SelectedItemChangedEventArgs e) => {
+                listView.SelectedItem = null;
+            };
+        }
+    }
+}
--- GodSpeak.Mobile/GodSpeak/Pages/MessagePage.xaml.cs
+++ GodSpeak.Mobile/GodSpeak/Pages/MessagePage.xaml.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using GodSpeak.Extensions;
 
 namespace GodSpeak
 {
-	public partial class MessagePage : ContentPage
-	{
-		public MessagePage()
-		{
-			InitializeComponent();
-			NavigationPage.SetHasNavigationBar(this, false);
-		}
-	}
+    public partial class MessagePage : ContentPage
+    {
+        public MessagePage ()
+        {
+            InitializeComponent ();
+            NavigationPage.SetHasNavigationBar (this, false);
+            MessagesListView.DeselectOnTap ();
+        }
+    }
 }
