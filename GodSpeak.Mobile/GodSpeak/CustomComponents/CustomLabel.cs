366c8062391e4c8d9b2f4c927061abd6505e082b
pauloortins <paulo.ortins@gmail.com>
2017-03-17 20:24:18 -0300

    Custom Styles

---
+++ GodSpeak.Mobile/GodSpeak/CustomComponents/CustomLabel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GodSpeak
+{
+	public class CustomLabel : Label
+	{
+		public enum FontWeights
+		{
+			Regular,
+			Bold,
+			Semibold,
+			Light,
+			Heavy
+		}
+
+		public static readonly BindableProperty FontWeightProperty =
+			BindableProperty.Create<CustomLabel, FontWeights>(
+				p => p.FontWeight, FontWeights.Regular, BindingMode.TwoWay, propertyChanged: OnFontWeightChanged);
+
+		public FontWeights FontWeight
+		{
+			get { return (FontWeights)this.GetValue(FontWeightProperty); }
+			set { this.SetValue(FontWeightProperty, value); }
+		}
+
+		private static void OnFontWeightChanged(BindableObject bindable, FontWeights oldvalue, FontWeights newValue)
+		{
+			var entry = (CustomLabel)bindable;
+			entry.FontWeight = newValue;
+		}
+
+		public CustomLabel()
+		{
+		}
+	}
+}
---
+++ GodSpeak.Mobile/iOS/Renderers/CustomLabelRenderer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using GodSpeak;
+using GodSpeak.iOS;
+
+[assembly: ExportRendererAttribute(typeof(CustomLabel), typeof(CustomLabelRenderer))]
+
+namespace GodSpeak.iOS
+{
+	public class CustomLabelRenderer : LabelRenderer
+	{
+		public CustomLabel CustomLabel
+		{
+			get { return Element as CustomLabel; }
+		}
+
+		public CustomLabelRenderer()
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+			SetFontWeight();
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CustomLabel.FontWeightProperty.PropertyName)
+			{
+				SetFontWeight();
+			}
+		}
+
+		private void SetFontWeight()
+		{
+			switch (this.CustomLabel.FontWeight)
+			{
+				case CustomLabel.FontWeights.Light:
+					this.Control.Font = UIFont.SystemFontOfSize((nfloat)CustomLabel.FontSize, UIFontWeight.Light);
+					break;
+				case CustomLabel.FontWeights.Regular:
+					this.Control.Font = UIFont.SystemFontOfSize((nfloat)CustomLabel.FontSize, UIFontWeight.Regular);
+					break;
+				case CustomLabel.FontWeights.Semibold:
+					this.Control.Font = UIFont.SystemFontOfSize((nfloat)CustomLabel.FontSize, UIFontWeight.Semibold);
+					break;
+				case CustomLabel.FontWeights.Bold:
+					this.Control.Font = UIFont.SystemFontOfSize((nfloat)CustomLabel.FontSize, UIFontWeight.Bold);
+					break;
+				case CustomLabel.FontWeights.Heavy:
+					this.Control.Font = UIFont.SystemFontOfSize((nfloat)CustomLabel.FontSize, UIFontWeight.Heavy);
+					break;
+			}
+		}
+
+		public override void LayoutSubviews()
+		{
+			base.LayoutSubviews();
+		}
+	}
+}
