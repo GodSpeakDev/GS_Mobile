2b1ec76f34138d6eb3ddc5adea936fdc8de63c91
pauloortins <paulo.ortins@gmail.com>
2017-03-28 21:35:37 -0300

    Working on Message Settings

---
+++ GodSpeak.Mobile/Droid/Renderers/PlusButtonRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using GodSpeak;
+using GodSpeak.Droid;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+
+[assembly: ExportRendererAttribute(typeof(PlusButton), typeof(PlusButtonRenderer))]
+namespace GodSpeak.Droid
+{
+	public class PlusButtonRenderer : ButtonRenderer
+	{
+		private GradientDrawable _drawable;
+		private GradientDrawable Drawable
+		{
+			get
+			{
+				if (_drawable == null)
+				{
+					_drawable = new GradientDrawable();
+					this.Control.SetBackground(_drawable);
+				}
+
+				return _drawable;
+			}
+		}
+
+		public PlusButtonRenderer()
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged(e);
+			Drawable.SetColor(Xamarin.Forms.Color.Transparent.ToAndroid());
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+		}
+	}
+}
---
+++ GodSpeak.Mobile/GodSpeak/CustomComponents/PlusButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GodSpeak
+{
+	public class PlusButton : Button
+	{
+		public PlusButton()
+		{
+		}
+	}
+}
