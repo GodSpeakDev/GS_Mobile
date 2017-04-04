e5722640f260c08261d90fe7e579e5da95803ddc
pauloortins <paulo.ortins@gmail.com>
2017-04-04 18:03:19 -0300

    #61:Register/Profile Screens - Image Size

---
+++ GodSpeak.Mobile/GodSpeak/Converters/ImageLayoutOptionsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace GodSpeak
+{
+	public class ImageLayoutOptionsConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null || (value is string && ((string)value).Contains("placeholder")))
+			{
+				return LayoutOptions.Center;
+			}
+
+			return LayoutOptions.Fill;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
