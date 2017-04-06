233d297a88f2d809275c91df56aa5faf7116a9fa
Ben Bishop <ben@rendr.io>
2017-04-06 15:19:33 -0500

    Text tweaks.

--- GodSpeak.Mobile/iOS/Renderers/CustomLabelRenderer.cs
+++ GodSpeak.Mobile/iOS/Renderers/CustomLabelRenderer.cs
@@ -4,44 +4,49 @@ using Xamarin.Forms.Platform.iOS;
 using UIKit;
 using GodSpeak;
 using GodSpeak.iOS;
+using Foundation;
 
-[assembly: ExportRendererAttribute(typeof(CustomLabel), typeof(CustomLabelRenderer))]
+[assembly: ExportRendererAttribute (typeof (CustomLabel), typeof (CustomLabelRenderer))]
 namespace GodSpeak.iOS
 {
-	public class CustomLabelRenderer : LabelRenderer
-	{
-		public CustomLabel CustomLabel
-		{
-			get { return Element as CustomLabel; }
-		}
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabel CustomLabel {
+            get { return Element as CustomLabel; }
+        }
 
-		public CustomLabelRenderer()
-		{
-		}
+        public CustomLabelRenderer ()
+        {
+        }
 
-		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
-		{
-			base.OnElementChanged(e);
-			SetFontWeight();
-		}
+        protected override void OnElementChanged (ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged (e);
+            SetFontWeight ();
+        }
 
-		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged(sender, e);
-			SetFontWeight();
-		}
+        protected override void OnElementPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged (sender, e);
+            SetFontWeight ();
+        }
 
-		private void SetFontWeight()
-		{
-			if (this.Control == null || this.CustomLabel == null)
-				return;
+        private void SetFontWeight ()
+        {
+            if (this.Control == null || this.CustomLabel == null)
+                return;
 
-			this.Control.Font = this.CustomLabel.GetUIFont();
-		}
 
-		public override void LayoutSubviews()
-		{
-			base.LayoutSubviews();
-		}
-	}
+            this.Control.Font = this.CustomLabel.GetUIFont ();
+
+
+        }
+
+
+
+        public override void LayoutSubviews ()
+        {
+            base.LayoutSubviews ();
+        }
+    }
 }
