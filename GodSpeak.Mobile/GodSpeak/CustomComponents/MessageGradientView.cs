78d4bce1f773c5d680fb71c680cda3b1d44d4fd9
pauloortins <paulo.ortins@gmail.com>
2017-04-08 07:14:21 -0300

    #71:[Android] - Messages List

---
+++ GodSpeak.Mobile/GodSpeak/CustomComponents/MessageGradientView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+namespace GodSpeak
+{
+	public class MessageGradientView : GradientBoxView
+	{
+		public MessageGradientView()
+		{
+			if (Device.RuntimePlatform == Device.iOS)
+			{
+				Colors = new Color[]
+				{
+					ColorHelper.IosDarkGrayGradient,
+					ColorHelper.IosLightGrayGradient,
+				};
+			}
+			else
+			{
+				Colors = new Color[]
+				{
+					Color.FromRgb(92, 94, 96)
+				};
+			}
+
+			BackgroundColor = Color.Transparent;
+		}
+	}
+}
