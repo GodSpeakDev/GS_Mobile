1e66b6bf8ae87753cda4d7052c6e75d68000de90
pauloortins <paulo.ortins@gmail.com>
2017-07-26 17:12:10 -0300

    #139:iOS and Android - Main Menu "Share Your Gifts" Buttons

---
+++ GodSpeak.Mobile/GodSpeak/MvvmCross/Messages/ShowActionMenuMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MvvmCross.Plugins.Messenger;
+
+namespace GodSpeak
+{
+	public class ShowActionMenuMessage : MvxMessage
+	{
+		public ShowActionMenuMessage(object sender) : base(sender)
+		{
+			
+		}
+	}
+}
