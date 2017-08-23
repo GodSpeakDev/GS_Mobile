40e578d7a590df96283a865e1039724befc49845
pauloortins <paulo.ortins@gmail.com>
2017-08-23 18:00:15 -0300

    #148: iOS and Android - Implement Reach Out

---
+++ GodSpeak.Mobile/Droid/Services/SmsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using GodSpeak.Services;
+
+namespace GodSpeak.Droid.Services
+{
+	public class SmsService : ISmsService
+	{
+		public void SendMessage(string message)
+		{
+			var uri = Android.Net.Uri.Parse("smsto:");
+			var intent = new Intent(Intent.ActionSendto, uri);
+			intent.PutExtra("sms_body", message);
+
+			(Xamarin.Forms.Forms.Context as Activity).StartActivity(intent);
+		}
+	}
+
+}
