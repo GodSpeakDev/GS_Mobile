076d5cf1d705af20345022b02d70fb67b8665e14
pauloortins <paulo.ortins@gmail.com>
2017-09-11 19:58:23 -0300

    #150: iOS and Android - Submit Bug/Feedback

--- GodSpeak.Mobile/iOS/Services/FeedbackService.cs
+++ GodSpeak.Mobile/iOS/Services/FeedbackService.cs
@@ -8,21 +8,33 @@ namespace GodSpeak.iOS.Services
     {
 		private IMessageService _messageService;
 		private IFileService _fileService;
+		private IMailService _mailService;
 
-        public FeedbackService (IMessageService messageService, IFileService fileService)
+		public FeedbackService(IMessageService messageService, IFileService fileService, IMailService mailService)
         {
 			_messageService = messageService;
 			_fileService = fileService;
+			_mailService = mailService;
         }
 
         public void OpenFeedbackDialog ()
         {
-			var deliveredMessages = NSData.FromFile(_fileService.GetFilePath(_messageService.DeliveredMessagesFile));
-			var upcomingMessages = NSData.FromFile(_fileService.GetFilePath(_messageService.UpcomingMessagesFile));
-			var log = NSData.FromFile(_fileService.GetFilePath("Log.txt"));
+			//var deliveredMessages = NSData.FromFile(_fileService.GetFilePath(_messageService.DeliveredMessagesFile));
+			//var upcomingMessages = NSData.FromFile(_fileService.GetFilePath(_messageService.UpcomingMessagesFile));
+			//var log = NSData.FromFile(_fileService.GetFilePath("Log.txt"));
 
-			var data = new NSObject[] { deliveredMessages, upcomingMessages, log};
-			BITHockeyManager.SharedHockeyManager.FeedbackManager.ShowFeedbackComposeViewWithPreparedItems(data);
+			//var data = new NSObject[] { deliveredMessages, upcomingMessages, log};
+			//BITHockeyManager.SharedHockeyManager.FeedbackManager.ShowFeedbackComposeViewWithPreparedItems(data);
+
+			_mailService.SendMail(
+				to: new string[] { "support@givegodspeak.com" },
+				subject: "[iOS] GodSpeak Feedback", 
+				files: new string[] 
+				{
+					_fileService.GetFilePath(_messageService.DeliveredMessagesFile),
+					_fileService.GetFilePath(_messageService.UpcomingMessagesFile),
+					_fileService.GetFilePath("Log.txt")
+				});
         }
     }
 }
